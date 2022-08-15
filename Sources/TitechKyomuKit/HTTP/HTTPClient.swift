20eeff7053e84ee4d81daf0b8ddf5b114d9d866c
nanashiki <nanashiki.app@gmail.com>
2022-08-16 01:14:36 +0900

    Fix for linux

--- Sources/TitechKyomuKit/HTTP/HTTPClient.swift
+++ Sources/TitechKyomuKit/HTTP/HTTPClient.swift
@@ -5,48 +5,49 @@ import FoundationNetworking
 
 protocol HTTPClient {
     func send(_ request: HTTPRequest) async throws -> String
-    func statusCode(_ request: HTTPRequest) async throws -> Int
 }
 
 struct HTTPClientImpl: HTTPClient {
     private let urlSession: URLSession
+    #if !canImport(FoundationNetworking)
     private let urlSessionDelegate: URLSessionTaskDelegate
-    private let urlSessionDelegateWithoutRedirect: URLSessionTaskDelegate
+    #endif
     private let userAgent: String
 
     init(urlSession: URLSession, userAgent: String) {
         self.urlSession =  urlSession
+        #if !canImport(FoundationNetworking)
         self.urlSessionDelegate = HTTPClientDelegate()
-        self.urlSessionDelegateWithoutRedirect = HTTPClientDelegateWithoutRedirect()
+        #endif
         self.userAgent = userAgent
     }
 
     func send(_ request: HTTPRequest) async throws -> String {
-        let (data, _) = try await urlSession.data(
-            for: request.generate(userAgent: userAgent),
-               delegate: urlSessionDelegate
-        )
+        let data = try await fetchData(request: request.generate(userAgent: userAgent))
 
         return String(data: data, encoding: .utf8) ?? ""
     }
     
-    func statusCode(_ request: HTTPRequest) async throws -> Int {
-        let (_, response) = try await urlSession.data(
-            for: request.generate(userAgent: userAgent),
-               delegate: urlSessionDelegateWithoutRedirect
-        )
-
-        return (response as? HTTPURLResponse)?.statusCode ?? 0
+    func fetchData(request: URLRequest) async throws -> Data {
+        #if canImport(FoundationNetworking)
+        return try await withCheckedThrowingContinuation { continuation in
+            urlSession.dataTask(with: request) { data, _, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: data ?? Data())
+                }
+            }.resume()
+        }
+        #else
+        return try await urlSession.data(for: request, delegate: urlSessionDelegate).0
+        #endif
     }
 }
 
 struct HTTPClientMock: HTTPClient {
     func send(_ request: HTTPRequest) async throws -> String {
         ""
-    }
-    
-    func statusCode(_ request: HTTPRequest) async throws -> Int {
-        0
     }
 }
 
@@ -78,37 +79,6 @@ class HTTPClientDelegate: URLProtocol, URLSessionTaskDelegate {
             print("  requestHeader: \(task.currentRequest!.allHTTPHeaderFields ?? [:])")
             print("  requestBody: \(String(data: task.originalRequest!.httpBody ?? Data(), encoding: .utf8) ?? "")")
             print("")
-        #endif
-    }
-}
-
-class HTTPClientDelegateWithoutRedirect: URLProtocol, URLSessionTaskDelegate {
-    func urlSession(
-        _: URLSession,
-        task: URLSessionTask,
-        willPerformHTTPRedirection response: HTTPURLResponse,
-        newRequest _: URLRequest,
-        completionHandler: @escaping (URLRequest?) -> Swift.Void
-    ) {
-        #if DEBUG
-            print("")
-            print("\(response.statusCode) \(task.currentRequest?.httpMethod ?? "") \(task.currentRequest?.url?.absoluteString ?? "")")
-            print("  requestHeader: \(task.currentRequest?.allHTTPHeaderFields ?? [:])")
-            print("  requestBody: \(String(data: task.originalRequest?.httpBody ?? Data(), encoding: .utf8) ?? "")")
-            print("")
-        #endif
-        completionHandler(nil)
-    }
-
-    func urlSession(_: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
-        #if DEBUG
-            if metrics.redirectCount == 0 {
-                print("")
-                print("200 \(task.currentRequest?.httpMethod ?? "") \(task.currentRequest?.url?.absoluteString ?? "")")
-                print("  requestHeader: \(task.currentRequest?.allHTTPHeaderFields ?? [:])")
-                print("  requestBody: \(String(data: task.originalRequest?.httpBody ?? Data(), encoding: .utf8) ?? "")")
-                print("")
-            }
         #endif
     }
 }
